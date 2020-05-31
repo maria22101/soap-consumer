3bcd8b76cf79c0e286ca648e0094bc66f25a286c
Mariia Bilous <maria.bilous101@gmail.com>
2020-06-01 02:36:23 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+**Spring boot training project**
+
+---------------------------------------------------------------------
+The project is to practice building SOAP-consuming web service
---
+++ src/test/java/com/lecture/soapconsumer/SoapConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lecture.soapconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
